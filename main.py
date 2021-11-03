4adfcbb97a7c1212b36f3303bdb267df3c5180b0
amazzing bitch <71969370+amazzingbitch@users.noreply.github.com>
2021-11-03 20:13:23 +0000

    осталось изменить таймеры, всё оки

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from functools import reduce
 import numpy as np
-from operator import mul, sub
 import timeit
 
 a = [[5, 3, 0, 0],
-     [3, 6, 1, 0],
+     [3, 8, 1, 0], # на позиции 8 должна быть 6
      [0, 1, 4, -2],
      [0, 0, 1, -3]]
-
 b = [8, 10, 3, -2]
+n = len(a)
 c = []
-x = np.zeros(4)
+x = np.zeros(n)
 eps = 10e-10
 
 def p_method(matrix):
@@ -27,50 +25,22 @@ def p_method(matrix):
         g[i] = 0
       else: g[i] = matrix[i][i+1]
       b[i] = -matrix[i][i]
+      d[i] = matrix[i][n]
       if i == 0:
         a[i] = 0
       else: a[i] = matrix[i][i-1]  
-    #g = [matrix[0][1], matrix[1][2], matrix[2][3], 0]
-    #b = [-matrix[0][0], -matrix[1][1], -matrix[2][2], -matrix[3][3]]
-    #a = [0, matrix[1][0], matrix[2][1], matrix[3][2]]
-    #d = [matrix[0][4], matrix[1][4], matrix[2][4], matrix[3][4]]
-    #p1 = g[0]/b[0]
-    #q1 = -d[0]/b[0]
     p[0] = g[0]/b[0]
     q[0] = -d[0]/b[0]
-    for i in range(n):
+    for i in range(1, n):
       if i != n-1 :
-        p[i+1] = g[i+1]/(b[i+1]-a[i+1]*p[i])
-        q[i+1] = (a[i+1]*q[i]-d[i+1])/(b[i+1]-a[i+1]*p[i+1])
-      else: q[i+1] = (a[i+1]*q[i]-d[i+1])/(b[i+1]-a[i+1]*p[i+1])
-    #p2 = g[1]/(b[1]-a[1]*p1)
-    #q2 = (a[1]*q1-d[1])/(b[1]-a[1]*p1)
-    #p3 = g[2]/(b[2]-a[2]*p2)
-    #q3 = (a[2]*q2-d[2])/(b[2]-a[2]*p2)
-    #q4 = (a[3]*q3-d[3])/(b[3]-a[3]*p3)
-    for i in range(n-1, 0):
-      if n-1:
-        x[i] = q[i]
-      else:
-        x[i] = p[i]*x[i-1]+q[i]
-
-    #x[3] = q4
-    #x[2] = p3*x[3]+q3
-    #x[1] = p2*x[2]+q2
-    #x[0] = p1*x[1]+q1
+        p[i] = g[i]/(b[i]-a[i]*p[i-1])
+      q[i] = (a[i]*q[i-1]-d[i])/(b[i]-a[i]*p[i-1])
+    x[n-1] = q[n-1]
+    for i in range(n-2, -1,-1):
+        x[i] = p[i]*x[i+1]+q[i]
+    for i in range(n):
+      x[i] = float("%.5f" % x[i])
     return x
-
-def sign(indexes):
-    s = sum((0, 1)[x < y] for k, y in enumerate(indexes) for x in indexes[k + 1:])
-    return (s + 1) % 2 - s % 2
-
-def column(row):
-    i = 0
-    n = len(row)
-    while not row[i] and i < n:
-        i += 1
-    return i if i < n else -1
-
 
 def kramer_method(les):
   n = len(les)
@@ -78,11 +48,9 @@ def kramer_method(les):
   tmp = list(zip(*les))
   b = tmp[-1]
   del tmp[-1]
-
   delta = np.linalg.det(tmp)
   if not delta:
       raise RuntimeError("No solution")
-
   result = []
   for i in range(n):
     a = tmp[:]
@@ -91,7 +59,6 @@ def kramer_method(les):
     x[i] = result[i]
     x[i] = float("%.5f" % x[i])
   return x
-
 
 def iteration_method(a, b, eps):
     n = len(a)
@@ -110,7 +77,8 @@ def iteration_method(a, b, eps):
                 max = i + 1
         condition = abs(x_new[max] - x[max]) < eps
         x = x_new
-
+    for i in range(n):
+      x[i] = float("%.5f" % x[i])
     return x
 
 for i in range(len(a)):
